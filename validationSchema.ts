fe3196a1a4a7e0642d87740f22c9bd312ce358dd
Haruna Ohkubo(Baru) <62496222+zebrawolf715@users.noreply.github.com>
2023-11-15 16:40:17 +0900

    説明追加
    確認メールアドレス
    確認パスワード

---
+++ validationSchema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const validationSchema = z
+    .object({
+        name: z.string().min(4, "名前は4文字以上で入力してください"),
+        email: z.string().email("正しいメールアドレスを入力してください"),
+        emailConfirmation: z.string(),
+        password: z.string().min(6, "パスワードは6文字以上で入力してください"),
+        passwordConfirmation: z.string(),
+    })
+    //確認メールアドレス
+    .refine(
+        ({ email, emailConfirmation }) => email === emailConfirmation,
+        { path: ["emailConfirmation"]}
+    )
+    //確認パスワード
+    .refine(
+        ({ password, passwordConfirmation }) => password === passwordConfirmation,
+        { path: ["passwordConfirmation"],}
+    );
+
